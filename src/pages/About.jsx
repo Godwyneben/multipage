e32638b10fc3797e9c689378b5bfaf0d1699756f
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-08-10 01:34:57 +0100

    router installed and added to various  paths

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Asperiores recusandae vel iusto, perferendis earum inventore, 
+          fuga, odit ipsa incidunt suscipit doloremque nobis. 
+          Nobis reprehenderit facere reiciendis assumenda, veritatis minus adipisci!
+        </p>
+    </div>
+  )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <div>
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Asperiores recusandae vel iusto, perferendis earum inventore, 
+          fuga, odit ipsa incidunt suscipit doloremque nobis. 
+          Nobis reprehenderit facere reiciendis assumenda, veritatis minus adipisci!
+        </p>
+    </div>
+  )
+}
