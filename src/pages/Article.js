fb8addb67039de50fe62d5cea7f6c59eaf5b12ce
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-08-14 23:39:34 +0100

    auto redirect when data is not found

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,22 @@
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 import { useFetch } from "../Hooks/useFetch"
+import { useEffect } from "react"
 
 
 export default function Article() {
   const { id } = useParams()
   const url = "http://localhost:3000/articles/" + id
   const { data: article, isPending, error } = useFetch(url)
+  const history = useHistory
+
+  useEffect(() => {
+    if (error){
+      // redirect 
+      setTimeout(() => {
+        history.push('/')
+      }, 2000)
+    }
+  }, [error])
 
   return (
     <div>
